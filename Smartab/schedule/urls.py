234780321829e110ef684ac09952b843798e7ab1
HAXF13D <vovik0312@gmail.com>
2021-11-13 20:09:38 +0300

    Add base site

--- Smartab/schedule/urls.py
+++ Smartab/schedule/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 
 urlpatterns = [
-
+    path('', views.todo_list, name='todo_list'),
+    path('edit', views.edit, name='edit'),
+    path('friends', views.friends, name='friends'),
+    path('share', views.share, name='share'),
+    path('settings', views.settings, name='settings'),
+    path('authorization', views.authorization, name='authorization'),
 ]
--- Smartab/schedule/views.py
+++ Smartab/schedule/views.py
@@ -1,3 +1,115 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def todo_list(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    data = {"title": "Список дел",
+            "text": "Главная страница",
+            "buttons_name": [
+                'todo_list',
+                'edit',
+                'friends',
+                'share',
+                'settings'
+            ]}
+    return render(
+        request,
+        'index.html',
+        context=data,
+    )
+
+
+def edit(request):
+    """
+    Функция отображения для редактора списка дел.
+    """
+    data = {"title": "Редактировать список дел",
+            "text": "Редактировать список дел",
+            "buttons_name": [
+                'todo_list',
+                'friends',
+                'share',
+                'settings'
+            ]}
+    return render(
+        request,
+        'index.html',
+        context=data,
+    )
+
+
+def friends(request):
+    """
+    Функция отображения для писка друзей.
+    """
+    data = {"title": "Список друзей",
+            "text": "Список друзей",
+            "buttons_name": [
+                'todo_list',
+                'edit',
+                'share',
+                'settings'
+            ]}
+    return render(
+        request,
+        'index.html',
+        context=data,
+    )
+
+
+def share(request):
+    """
+    Функция отображения для страницы с возможность отправить список дела в соц сети.
+    """
+    data = {"title": "Поделиться списком",
+            "text": "Поделиться списком",
+            "buttons_name": [
+                'todo_list',
+                'edit',
+                'friends',
+                'settings'
+            ]}
+    return render(
+        request,
+        'index.html',
+        context=data,
+    )
+
+
+def settings(request):
+    """
+    Функция отображения для окна настроек.
+    """
+    data = {"title": "Настройки",
+            "text": "Тут будут настройки",
+            "buttons_name": [
+                'todo_list',
+                'edit',
+                'friends',
+                'share',
+            ]}
+    return render(
+        request,
+        'index.html',
+        context=data,
+    )
+
+
+def authorization(request):
+    """
+    Функция отображения для окна авторизации.
+    """
+    data = {"title": "Войти",
+            "text": "Тут будет окно входа",
+            "buttons_name": [
+                'todo_list',
+            ]}
+    return render(
+        request,
+        'index.html',
+        context=data,
+    )
